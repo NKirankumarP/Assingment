11aed8511e242d1b89b112d16afd3f3add3118c0
NKirankumarP <140386673+NKirankumarP@users.noreply.github.com>
2023-07-24 14:26:32 +0530

    Add files via upload

---
+++ class4.R
@@ -0,0 +1,43 @@
+a < - 100
+class(a)
+a <- 100 
+class(a)
+a <- 100L
+class(a)
+b <- 10.5
+class(b)
+age <- "is a number"
+class(age)
+bool <- T
+class(bool)
+char <- "10+5+5+10"
+class(char)
+char <- 10+5+5+5
+class(char)
+x = 100
+y = 50
+a = x+y
+print(a)
+getwd()
+
+
+#data structures 
+
+Num <- c(100, 20, 30, 40)
+print(Num)
+
+Char <- c( 'ram', 'sam', 'tom')
+Char
+
+char <- c(85, 'ram', 'tom')
+class(char)
+a <- (5,5,3,5)
+
+a <- c('@', '#', '$')
+print (a)
+a[1]
+a[1:3]
+char[2]
+char[3]
+Char <- char
+Char [1]
